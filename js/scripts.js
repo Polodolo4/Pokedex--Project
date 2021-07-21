4179187f34dfd1188594a80841ca07c3eb917097
Polodolo4 <86023627+Polodolo4@users.noreply.github.com>
2021-07-20 21:14:19 -0400

    Added 4 Pokémon to scripts.js
    added 4 pokemon objects to pokemonList.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-
+let pokemonList = [
+  {
+    name: 'Butterfree',
+    height: 1.1,
+    type: ['bug', 'flying'],
+    weight: 32,
+  },
+  {
+    name: 'Starmie',
+    height: 1.1,
+    type: ['psychic', 'water'],
+    weight: 80,
+  },
+  {
+    name: 'Omastar',
+    height: 1,
+    type: ['water', 'rock'],
+    weight: 35,
+  },
+  {
+    name: 'Onix',
+    height: 8.8,
+    type: ['rock', 'ground'],
+    weight: 210,
+  },
+];
